77ae664b1bcd2ea9213e7ee2b6d641d2aed32bb5
RomainCoudour <rom.coudour@gmail.com>
2024-01-23 18:44:58 +0100

    chore: update project configuration

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import app from './src/app';
 
 const server = app.listen(app.get('port'), () => {
   console.log(
-    '  App is running at http://localhost:%d in %s mode',
+    'App is running at http://localhost:%d in %s mode',
     app.get('port'),
     app.get('env'),
   );
